04edf5ddb94a4a5383ce6a238a8d3dd83d146a89
ArtusU <artusrock@hotmail.com>
2021-06-19 10:40:57 +0100

    signup

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -85,6 +85,30 @@ export const checkAuthenticated = () => async dispatch => {
 };
 
 
+export const signup = (first_name, last_name, email, password, re_password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ first_name, last_name, email, password, re_password });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/`, body, config);
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL
+        })
+    }
+};
+
+
 export const login = (email, password) => async dispatch => {
     const config = {
         headers: {
